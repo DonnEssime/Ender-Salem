a5053cdf93d17483475e2d26b580982b4aa57ce3
Simon Donne <donnesimon@gmail.com>
2014-10-22 00:10:42 +0200

    Also added 64 bit libraries, tweaked lachlaan's compile script to copy requisite libraries to the build folder

--- compile.bat
+++ compile.bat
@@ -1,5 +1,7 @@
 ant
 echo "DONE"
+cp lib/gluegen-*.jar build/
+cp lib/jogl-*.jar build/
 cd build
 echo "BOOM DONE AGAIN"
 java.exe -Dhaven.resurl="http://game.salemthegame.com/res/" -Dhaven.defserv="game.salemthegame.com" -Xmx1024m -jar salem.jar -P
